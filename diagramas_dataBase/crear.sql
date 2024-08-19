dc55427842c2510a1cacd6ca3fe35e7c6b1c641d
kleber chavez <kleberenriqueos@gmail.com>
2024-08-18 21:40:46 -0500

    correccion database

--- diagramas_dataBase/crear.sql
+++ diagramas_dataBase/crear.sql
@@ -1,20 +1,26 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     15/8/2024 22:19:23                           */
+/* Created on:     18/8/2024 12:58:45                           */
 /*==============================================================*/
 
 
 drop table if exists CATEGORIA;
 
+drop table if exists CLIENTE;
+
 drop table if exists CONSUMIBLE;
 
 drop table if exists DESCUENTO;
 
+drop table if exists DETALLEENTREGA;
+
+drop table if exists DETALLEFACTURA;
+
+drop table if exists DETALLEVENTA;
+
+drop table if exists ENTREGA;
+
 drop table if exists FACTURACION;
-
-drop table if exists FICHAENTREGA;
-
-drop table if exists HISTORIAL;
 
 drop table if exists INVENTARIO;
 
@@ -39,6 +45,20 @@ create table CATEGORIA
 );
 
 /*==============================================================*/
+/* Table: CLIENTE                                               */
+/*==============================================================*/
+create table CLIENTE
+(
+   IDCIENTE             int not null auto_increment,
+   NOMBRE               varchar(32) not null,
+   APELLIDO             varchar(32) not null,
+   CEDULA               varchar(10) not null,
+   EMAIL                varchar(100) not null,
+   TELEFONO             varchar(10) not null,
+   primary key (IDCIENTE)
+);
+
+/*==============================================================*/
 /* Table: CONSUMIBLE                                            */
 /*==============================================================*/
 create table CONSUMIBLE
@@ -55,9 +75,59 @@ create table CONSUMIBLE
 create table DESCUENTO
 (
    IDDESCUENTO          int not null auto_increment,
+   IDPRODUCTO           int,
    NOMBREDES            varchar(32) not null,
    PORSENTAJE           numeric(8,0) not null,
    primary key (IDDESCUENTO)
+);
+
+/*==============================================================*/
+/* Table: DETALLEENTREGA                                        */
+/*==============================================================*/
+create table DETALLEENTREGA
+(
+   IDPRODUCTO           int not null,
+   IDPROVEEDOR          int not null,
+   IDENTREGA            int not null,
+   FECHA                datetime not null,
+   primary key (IDPRODUCTO, IDPROVEEDOR, IDENTREGA)
+);
+
+/*==============================================================*/
+/* Table: DETALLEFACTURA                                        */
+/*==============================================================*/
+create table DETALLEFACTURA
+(
+   IDDETALLFACTURA      int not null auto_increment,
+   IDFACTURA            int,
+   IDPRODUCTO           int,
+   IDSERVICIOS          int,
+   IDUSUARIO            int,
+   CANTIDAD             numeric(8,0),
+   PRECIOUNITARIO       decimal(10,2) not null,
+   primary key (IDDETALLFACTURA)
+);
+
+/*==============================================================*/
+/* Table: DETALLEVENTA                                          */
+/*==============================================================*/
+create table DETALLEVENTA
+(
+   IDPRODUCTO           int not null,
+   IDSERVICIOS          int not null,
+   IDUSUARIO            int not null,
+   FECHAHORA            datetime not null,
+   primary key (IDPRODUCTO, IDSERVICIOS, IDUSUARIO)
+);
+
+/*==============================================================*/
+/* Table: ENTREGA                                               */
+/*==============================================================*/
+create table ENTREGA
+(
+   IDENTREGA            int not null auto_increment,
+   FECHAHORA            datetime not null,
+   primary key (IDENTREGA)
 );
 
 /*==============================================================*/
@@ -66,36 +136,12 @@ create table DESCUENTO
 create table FACTURACION
 (
    IDFACTURA            int not null auto_increment,
-   IDPRODUCTO           int not null,
-   FECHAE               date not null,
+   IDCIENTE             int not null,
+   FECHA                date not null,
    PAGO                 decimal(10,2) not null,
    SUBTOTAL             decimal(10,2) not null,
    IMPUESTO             decimal(10,2) not null,
    primary key (IDFACTURA)
-);
-
-/*==============================================================*/
-/* Table: FICHAENTREGA                                          */
-/*==============================================================*/
-create table FICHAENTREGA
-(
-   IDPROVEEDOR          int not null,
-   IDPRODUCTO           int not null,
-   FECHAENTREGA         date,
-   primary key (IDPROVEEDOR, IDPRODUCTO)
-);
-
-/*==============================================================*/
-/* Table: HISTORIAL                                             */
-/*==============================================================*/
-create table HISTORIAL
-(
-   IDPRODUCTO           int not null,
-   IDSERVICIOS          int not null,
-   IDUSUARIO            int not null,
-   IDFACTURA            int not null,
-   FECHAHORA            datetime,
-   primary key (IDPRODUCTO, IDSERVICIOS, IDUSUARIO, IDFACTURA)
 );
 
 /*==============================================================*/
@@ -107,7 +153,6 @@ create table INVENTARIO
    IDPRODUCTO           int not null,
    STOCKMINIMO          int not null,
    ESTADOPRODUCTO       bool not null,
-   CANTIDAD             char(10),
    primary key (IDINVENTARIO)
 );
 
@@ -118,7 +163,6 @@ create table PRODUCTO
 (
    IDPRODUCTO           int not null auto_increment,
    IDCATEGORI           int not null,
-   IDDESCUENTO          int,
    NOMBREPRODUCTO       varchar(32) not null,
    DESCRIPCION          text not null,
    PRECIO               decimal(10,2) not null,
@@ -132,10 +176,11 @@ create table PRODUCTO
 create table PROVEEDOR
 (
    IDPROVEEDOR          int not null auto_increment,
-   NOMBREPRODUCTO       varchar(32) not null,
+   NOMBRE               varchar(32) not null,
    APELLIDO             varchar(32) not null,
    CI_RUC               varchar(12) not null,
    NOMBREEMPRESA        varchar(100) not null,
+   NOMBREPRODUCTO       varchar(32) not null,
    primary key (IDPROVEEDOR)
 );
 
@@ -157,7 +202,6 @@ create table SERVICIOS
    IDSERVICIOS          int not null auto_increment,
    IDCONSUMIBLES        int not null,
    NOMBRESERVICIO       varchar(100) not null,
-   NOMBRE               varchar(32) not null,
    DESCRIPCION          text not null,
    PRECIO               decimal(10,2) not null,
    primary key (IDSERVICIOS)
@@ -179,23 +223,35 @@ create table USUARIO
    primary key (IDUSUARIO)
 );
 
-alter table FICHAENTREGA add constraint FK_FICHAENTREGA foreign key (IDPRODUCTO)
+alter table DESCUENTO add constraint FK_SE_APLICA foreign key (IDPRODUCTO)
       references PRODUCTO (IDPRODUCTO) on delete restrict on update restrict;
 
-alter table FICHAENTREGA add constraint FK_FICHAENTREGA2 foreign key (IDPROVEEDOR)
+alter table DETALLEENTREGA add constraint FK_DEJA foreign key (IDPRODUCTO)
+      references PRODUCTO (IDPRODUCTO) on delete restrict on update restrict;
+
+alter table DETALLEENTREGA add constraint FK_ENTREGA foreign key (IDENTREGA)
+      references ENTREGA (IDENTREGA) on delete restrict on update restrict;
+
+alter table DETALLEENTREGA add constraint FK_RELATIONSHIP_7 foreign key (IDPROVEEDOR)
       references PROVEEDOR (IDPROVEEDOR) on delete restrict on update restrict;
 
-alter table HISTORIAL add constraint FK_HISTORIAL foreign key (IDFACTURA)
+alter table DETALLEFACTURA add constraint FK_FORMA foreign key (IDFACTURA)
       references FACTURACION (IDFACTURA) on delete restrict on update restrict;
 
-alter table HISTORIAL add constraint FK_HISTORIAL2 foreign key (IDPRODUCTO)
+alter table DETALLEFACTURA add constraint FK_RELACIONA foreign key (IDPRODUCTO, IDSERVICIOS, IDUSUARIO)
+      references DETALLEVENTA (IDPRODUCTO, IDSERVICIOS, IDUSUARIO) on delete restrict on update restrict;
+
+alter table DETALLEVENTA add constraint FK_DETALLE2 foreign key (IDPRODUCTO)
       references PRODUCTO (IDPRODUCTO) on delete restrict on update restrict;
 
-alter table HISTORIAL add constraint FK_HISTORIAL3 foreign key (IDSERVICIOS)
+alter table DETALLEVENTA add constraint FK_DETALLE3 foreign key (IDSERVICIOS)
       references SERVICIOS (IDSERVICIOS) on delete restrict on update restrict;
 
-alter table HISTORIAL add constraint FK_HISTORIAL4 foreign key (IDUSUARIO)
+alter table DETALLEVENTA add constraint FK_DETALLE4 foreign key (IDUSUARIO)
       references USUARIO (IDUSUARIO) on delete restrict on update restrict;
+
+alter table FACTURACION add constraint FK_RECIVE foreign key (IDCIENTE)
+      references CLIENTE (IDCIENTE) on delete restrict on update restrict;
 
 alter table INVENTARIO add constraint FK_ESTA_DETRO foreign key (IDPRODUCTO)
       references PRODUCTO (IDPRODUCTO) on delete restrict on update restrict;
@@ -203,12 +259,9 @@ alter table INVENTARIO add constraint FK_ESTA_DETRO foreign key (IDPRODUCTO)
 alter table PRODUCTO add constraint FK_PERTENECE foreign key (IDCATEGORI)
       references CATEGORIA (IDCATEGORI) on delete restrict on update restrict;
 
-alter table PRODUCTO add constraint FK_SE_APLICA foreign key (IDDESCUENTO)
-      references DESCUENTO (IDDESCUENTO) on delete restrict on update restrict;
-
 alter table SERVICIOS add constraint FK_CONSUME foreign key (IDCONSUMIBLES)
       references CONSUMIBLE (IDCONSUMIBLES) on delete restrict on update restrict;
 
-alter table USUARIO add constraint FK_RELATIONSHIP_8 foreign key (IDROL)
+alter table USUARIO add constraint FK_TIENE foreign key (IDROL)
       references ROL (IDROL) on delete restrict on update restrict;
 
