eda15a8e01ec42299632ee743fb21579e320de58
kleber chavez <kleberenriqueos@gmail.com>
2024-08-18 22:41:28 -0500

    verificacion usuario

--- php/login.php
+++ php/login.php
@@ -15,10 +15,16 @@ if ($conn->connect_error) {
 $sql = "SELECT IDUSUARIO, IDROL, NOMBREUSUARIO,CONTRASENA FROM usuario";
 $result = $conn->query($sql);
 
+
 if ($result->num_rows > 0) {
-  // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["IDUSUARIO"]. " - id rol: " . $row["IDROL"]. " " . $row["NOMBREUSUARIO"]. $row["CONTRASENA"]."<br>";
+    //echo "id: " . $row["IDUSUARIO"]. " - id rol: " . $row["IDROL"]. " " . $row["NOMBREUSUARIO"]. $row["CONTRASENA"]."<br>";
+    if($row["NOMBREUSUARIO"] == $_POST['username'] && $row["CONTRASENA"] == $_POST['password']){
+      echo "Bienvenido";
+      header("Location: ../prueba.html");
+    }else{
+      echo "Usuario o contraseña incorrectos";
+    }
   }
 } else {
   echo "0 results";
