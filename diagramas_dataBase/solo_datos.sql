49b9893cb0702a86fd326141f14119ba940b57d6
Kleber Enrique Chávez Oscullo <kleberenriqueos@gmail.com>
2024-08-25 20:39:04 -0500

    data

---
+++ diagramas_dataBase/solo_datos.sql
@@ -0,0 +1,7 @@
+INSERT INTO (ROL) VALUES ('Administrador'),('Vendedor'),('Bodeguero');
+-- Insertar un usuario administrador
+INSERT INTO usuario (IDROL, CEDULA, NOMBREU, APELLIDOU, CONTRASENA, CORREO, ESTADO)
+VALUES (1, '1234567890', 'Juan', 'Pérez', 'contrasena123', 'juan.perez@dominio.com', 'Activo'),
+(2, '0987654321', 'Ana', 'García', 'contrasena456', 'ana.garcia@dominio.com', 'Activo'),
+(3, '1122334455', 'Carlos', 'López', 'contrasena789', 'carlos.lopez@dominio.com', 'Activo');
+
---
+++ paginas/js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    // Cargar las cartas cuando la página se haya cargado
+    loadCards();
+
+    function loadCards() {
+        var idUsuario = localStorage.getItem("idUsuario");
+
+        $.ajax({
+            url: 'php/permisos.php', // Archivo PHP que devolverá los datos
+            type: 'POST', 
+            data: {
+                idUsuario: idUsuario
+            },
+            success: function(response) {
+                
+                var data = JSON.parse(response);
+                console.log(typeof data );
+                console.log(data);
+                // Verificar si la respuesta es válida y contiene datos
+                if (data.length > 0) {
+                    data.forEach(item => {
+                        // Crear la carta
+                        const card = document.createElement('a');
+                        card.classList.add('card');
+                        card.addEventListener('click', () => {
+                            window.location.href = item.link;
+                        });
+
+                        card.innerHTML = `
+                            <h3>${item.title}</h3>
+                        `;
+
+                        // Agregar la carta al contenedor
+                        document.getElementById('card-container').appendChild(card);
+                    });
+                } else {
+                    $('#card-container').append('<p>No hay cartas disponibles.</p>');
+                }
+            },
+            error: function(status, error) {
+                console.error("Error en la solicitud AJAX: ", status, error);
+                alert("Error al cargar las cartas");
+            }
+        });
+    }
+});
